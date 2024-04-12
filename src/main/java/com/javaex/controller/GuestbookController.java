a79dc8cd4a8f572ee4dd0fa6085887bb94019980
m-ji97 <tv5035@naver.com>
2024-04-12 11:18:16 +0900

    240412

--- src/main/java/com/javaex/controller/GuestbookController.java
+++ src/main/java/com/javaex/controller/GuestbookController.java
@@ -3,13 +3,11 @@ package com.javaex.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.javaex.service.GuestbookService;
@@ -24,7 +22,7 @@ public class GuestbookController {
 		
 	//리스트
 	//@ResponseBody
-	@RequestMapping(value = "/api/guests", method=RequestMethod.GET)
+	@GetMapping("/api/guests")
 	public List<GuestbookVo> list() {
 		System.out.println("GuestbookController.list()");
 		
@@ -37,7 +35,7 @@ public class GuestbookController {
 	
 	//저장+1개가져오기
 	//@ResponseBody
-	@RequestMapping(value = "/api/guests", method=RequestMethod.POST)
+	@PostMapping("/api/guests")
 	public String add(@RequestBody GuestbookVo guestbookVo) {
 		System.out.println("GuestbookController.add()");
 		System.out.println(guestbookVo);
@@ -62,7 +60,6 @@ public class GuestbookController {
 		String result = "{\"count\":"+count+"}";
 		
 		return result;
-		
 	}
 
 
